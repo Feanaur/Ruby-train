57b7f101b690182adb8777aa8100edaa781b09fd
Feanaur <feanaur2193@gmail.com>
2013-11-02 12:04:20 +0700

    Add module with lambda

--- Input_filters.rb
+++ Input_filters.rb
@@ -1,7 +1,6 @@
 module Input_filters
 
-  def self.integer_input()
-    yield
+  integer_input= lambda do
     input=gets.chomp
     while input!=input.to_i.to_s do
       print "\nWrong input. Please try again: "
