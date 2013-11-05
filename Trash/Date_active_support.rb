ad51d8ab414ebd64fe9c566b0c7eb4a71c49cb44
Feanaur <feanaur2193@gmail.com>
2013-11-05 22:32:29 +0700

    Cleaning

--- Trash/Date_active_support.rb
+++
@@ -1,9 +0,0 @@
-def anniversary(game, years)
-  game[:release].advance(years: years)
-end
-
-game = {
-  name: 'Contra',
-  release: DateTime.new(1987, 2, 20, 0, 0, 0)
-}
-puts anniversary(game, 20)
