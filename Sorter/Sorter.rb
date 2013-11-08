66133b8803628789a65e8db0dc24a5b49ae04dda
Feanaur <feanaur2193@gmail.com>
2013-11-08 22:26:28 +0700

    Add wrong input check and base for sorter class

--- Sorter/Sorter.rb
+++ Sorter/Sorter.rb
@@ -5,10 +5,35 @@ class Sorter
     @hashArray = array
   end
 
-  def missing_method()
-  @hashArray.each do |system|
-    define_method "sort_by_#{system.downcase}?" do
-      
+
+  def missing_method(method_name)
+    #name = method_name.to_s   
+
+    if method_name.index('sort_by_')==0
+      key_string=method_name[8..-1]
+
+        is_a_good_key = lambda do
+          @hashArray[0].each_key do |key|
+           if key.to_s==key_string 
+              return true
+            else
+              return false
+            end
+          end
+        end
+
+      if is_a_good_key do
+
+        ## Здесь будет сортировка по ключевому значению из key_string, 
+        ## хотя как её реализовывать я пока понятия не имею...
+      else
+        puts "Wrong key to sort"
+      end
+
+    else
+      puts "Error. This method doesn`t exist."
     end
   end
+
+
 end
